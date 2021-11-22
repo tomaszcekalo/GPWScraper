2dda944201ad3a6ff2443aa9834746ba2189d168
Tomasz Cekało <tomasz.cekalo@gmail.com>
2021-11-22 15:55:39 +0100

    Add project files.

---
+++ GPWScraper/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace GPWScraper;
+public class Class1
+{
+
+}
---
+++ GPWScraper/MainPageScraper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPWScraper
+{
+    public class MainPageScraper
+    {
+        //#rynek-naj-rosnace
+        //#rynek-naj-spadaja
+        /*
+        [id^=product]
+        ^= indicates "starts with". Conversely, $= indicates "ends with".
+
+        The symbols are actually borrowed from Regex syntax, where ^ and $ mean "start of string" and "end of string" respectively.
+
+        See the specs for full information.
+
+        */
+        //ph_rynek_naj_aktywne_chart
+        //#akcje
+        //#pochodne
+
+        //.gpw_style a
+
+    }
+}
